c60d8a8f47e278a2db2537c08c1680cf2524a9a7
PrashanthSingaravelan <prashanth_singar_hotmail.com>
2021-05-18 08:11:07 +0530

    18/5

---
+++ Abstract_Window_Toolkit/AWT_programs/src/event_handling/button_ActionListener.java
@@ -0,0 +1,39 @@
+package event_handling;
+
+import java.awt.*;
+import java.awt.event.*;
+
+class my_frame extends Frame implements ActionListener{    
+    int cnt=0;
+    Button b_obj;
+    Label l_obj;
+
+my_frame(String frame_name_main) {
+          super(frame_name_main);
+          l_obj   = new Label("                 " + cnt);
+          b_obj  = new Button("Click");
+          
+          // event must be registered with the listener
+          b_obj.addActionListener(this);    
+          // button(component) is now registered with the frame object (which itself act as a listener )since Frame class implements the ActionListener interface
+        
+          setLayout(new FlowLayout());
+          this.add(l_obj);
+          this.add(b_obj);
+        }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+         // what to do when the button is clicked ?????
+         cnt+=1;                                             // increment the button
+         l_obj.setText("                 "+ cnt);    // and then display the incremented value
+    }
+}
+
+public class button_ActionListener {
+    public static void main(String args[]) {
+            my_frame fr = new my_frame("Increment window");
+            fr.setSize(400,400);
+            fr.setVisible(true);
+    }
+}
---
+++ assignment_question/src/assignment_question/Assignment_question.java
@@ -0,0 +1,59 @@
+package assignment_question;
+        
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Date;
+import java.text.SimpleDateFormat;  
+import java.util.Locale;
+
+interface GlobalConstants {
+    SimpleDateFormat  date_format = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH); // SimpleDateFormat instance
+}
+
+class Donor implements GlobalConstants{
+         String name;
+         int age;
+         String address;
+         long num1;
+         String blood_group;
+         static Date donation_date;
+    
+        Donor(String name_main , int age_main , String address_main , long num1_main , String blood_group_main , String date_main) {
+            this.name             = name_main;
+            this.age                = age_main;
+            this.address         = address_main;
+            this.num1             = num1_main;
+            this.blood_group = blood_group_main;
+            try{ this.donation_date = date_format.parse(date_main); } catch (Exception e) { }             
+    }
+}
+        
+public class Assignment_question {
+    public static void main(String[] args) {
+         Donor[] obj1 = new Donor[4];
+         obj1[0] = new Donor("Prashanth" , 24 , "No:54, 14th street, Kodambakkam" , 9003075249L,   "B+ve" , "17/05/21") ; 
+         obj1[1] = new Donor("Kumar"       , 56 , "No:67, 18th street, Canada"             , 88795429382L, "A+ve" , "15/08/2020");
+         obj1[2] = new Donor("Pradeep"     , 71 , "No:79 20th street Belgium"             , 9952415524L,  "O+ve" , "4/06/2021");
+         obj1[3] = new Donor("Arun Kumar" , 39 , "No:56 19th street Kolkata"            , 8852466990L,   "B+ve" , "09/05/2021");
+         
+         // Object serialization
+         try{
+                   FileOutputStream fos      = new FileOutputStream("serial");
+                   ObjectOutputStream oos = new ObjectOutputStream(fos);
+                   for(int i=0;i<4;i++) { oos.writeObject(obj1[i]); }
+         } catch(Exception e) {}
+        
+       // Object deserialization
+        try {
+                   Donor[] obj2 = new Donor[4];
+                   FileInputStream fos      = new FileInputStream("serial");
+                   ObjectInputStream oos = new ObjectInputStream(fos);
+                   for(int i=0;i<4;i++) { obj2[i] = (Donor[i])ois.readObject(); }
+        } catch(Exception e) {}
+        
+        
+ 
+   }
+}
